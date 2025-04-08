950a313eba26e9a41bfb8080eedf007548553b62
Yippi <57821687+YPYP333YPYP@users.noreply.github.com>
2025-04-08 13:27:54 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,41 +1,48 @@
 n, m = map(int, input().split())
 r, c, d = map(int, input().split())
-room = [list(map(int, input().split())) for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]  # n개의 행 입력
 
-dr = [-1, 0, 1, 0]  
+result = 0
+
+# 북, 동, 남, 서 방향으로 이동 (문제 정의에 맞게)
+dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
-cleaned = 0
-
-cleaned_area = [row[:] for row in room]
-
 while True:
-    if cleaned_area[r][c] == 0:
-        cleaned_area[r][c] = 2
-        cleaned += 1
-    
-    has_unclean = False
-    
+    # 1. 현재 칸이 청소되지 않은 경우, 청소
+    if graph[r][c] == 0:
+        graph[r][c] = 2  # 청소된 칸은 2로 표시
+        result += 1
+
+    # 2. 주변 4칸 확인
+    cleaned = True
     for i in range(4):
-        nd = (d + 3) % 4
-        nr = r + dr[nd]
-        nc = c + dc[nd]
-        
-        if 0 <= nr < n and 0 <= nc < m and cleaned_area[nr][nc] == 0:
-            r, c, d = nr, nc, nd
-            has_unclean = True
-            break
-        
-        d = nd
-    
-    if not has_unclean:
-        back_d = (d + 2) % 4
-        nr = r + dr[back_d]
-        nc = c + dc[back_d]
-        
-        if 0 <= nr < n and 0 <= nc < m and cleaned_area[nr][nc] != 1:
-            r, c = nr, nc
-        else:
+        nr = r + dr[i]
+        nc = c + dc[i]
+        if 0 <= nr < n and 0 <= nc < m and graph[nr][nc] == 0:
+            cleaned = False
             break
 
-print(cleaned)
+    if cleaned:  # 주변 4칸이 모두 청소되었거나 벽인 경우
+        # 후진할 위치 계산
+        back_r = r + dr[(d + 2) % 4]
+        back_c = c + dc[(d + 2) % 4]
+
+        # 후진할 수 없다면 작동 멈춤
+        if back_r < 0 or back_r >= n or back_c < 0 or back_c >= m or graph[back_r][back_c] == 1:
+            break
+
+        # 후진
+        r, c = back_r, back_c
+    else:  # 청소되지 않은 칸이 있는 경우
+        # 반시계 방향으로 90도 회전
+        d = (d + 3) % 4
+
+        # 앞쪽 칸이 청소되지 않은 빈칸인 경우 전진
+        front_r = r + dr[d]
+        front_c = c + dc[d]
+
+        if 0 <= front_r < n and 0 <= front_c < m and graph[front_r][front_c] == 0:
+            r, c = front_r, front_c
+
+print(result)

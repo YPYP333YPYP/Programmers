bf939b814c460722d5b3f66181efa39c8f7569d2
Yippi <57821687+YPYP333YPYP@users.noreply.github.com>
2025-04-21 00:27:25 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,37 +2,35 @@ from collections import deque
 
 N, M = map(int, input().split())
 
-def bfs(x, y):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    visited = [[False] * M for _ in range(N)]
-    visited[x][y] = True
-    
+tmp = [list(map(str, input().split())) for _ in range(N)]
+graph = [[] * M for _ in range(N)]
+for i in range(N):
+    for v in tmp[i]:
+        for j in v:
+            graph[i].append(int(j))
+
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+visited = [[False] * M for _ in range(N)]
+dist = [[0]*M for _ in range(N)]
+
+def bfs(sx,sy):
     q = deque()
-    q.append((x, y, 1))
-    
+    q.append((sx,sy))
+    visited[sx][sy] = True
+    dist[sx][sy] = 1
+
     while q:
-        x, y, dist = q.popleft()
-        
-        if x == N-1 and y == M-1:
-            return dist
-        
+        x,y = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if 0 <= nx < N and 0 <= ny < M:
-                if graph[nx][ny] == 1 and visited[nx][ny] == False:
-                    visited[nx][ny] = True
-                    q.append((nx, ny, dist + 1))
-    return -1
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx,ny))
+                dist[nx][ny] = dist[x][y] + 1
 
-graph = []
 
-for _ in range(N):
-    graph.append(list(map(int, input())))
-
-print(bfs(0,0))
-
-    
+bfs(0,0)
+print(dist[N-1][M-1])

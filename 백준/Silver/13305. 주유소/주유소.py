582488bae025c290e6f1098cb8a7c08047ab728e
Yippi <57821687+YPYP333YPYP@users.noreply.github.com>
2025-05-29 21:06:19 +0900

    [Silver III] Title: 주유소, Time: 136 ms, Memory: 136728 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -10,12 +10,11 @@ money = 0
 for i in range(N-1):
     if i == 0:
         money = node[i]
-        result += money * d[i]
     else:
-        if node[i-1] > node[i]:
+        if money > node[i]:
             money = node[i]
 
-        result += money * d[i]
+    result += money * d[i]
 
 print(result)
 

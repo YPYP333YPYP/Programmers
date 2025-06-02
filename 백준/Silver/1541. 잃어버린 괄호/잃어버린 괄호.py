ff6bce1dbccb37741030776f5cbfc6abb55e92a8
Yippi <57821687+YPYP333YPYP@users.noreply.github.com>
2025-06-03 02:39:53 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,10 @@
+string = input()
+
+parts = string.split('-')
+
+result = sum(map(int, parts[0].split('+')))
+
+for i in range(1, len(parts)):
+    result -= sum(map(int, parts[i].split('+')))
+
+print(result)

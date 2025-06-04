c3634e74698e8c6649dd557efa2ed00cfe4e7b99
Yippi <57821687+YPYP333YPYP@users.noreply.github.com>
2025-06-04 20:57:56 +0900

    [Gold IV] Title: 인구 이동, Time: 1064 ms, Memory: 133932 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,45 +1,65 @@
 from collections import deque
-n, l, r = map(int, input().split())
+
+N, L, R = map(int, input().split())
+
 graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
-    
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-def bfs(x, y, visited):
-    q = deque()
-    q.append((x,y))
-    
+
+for i in range(N):
+    row = list(map(int, input().split()))
+    graph.append(row)
+
+
+
+def bfs(x,y,v):
+    queue = deque()
+    queue.append((x,y))
+    v[x][y] = True
     union = []
     union.append((x,y))
-    visited[x][y] = True
-    while q:
-        a, b = q.popleft()
-        
-        for i in range(4):
-            na = a + dx[i]
-            nb = b + dy[i]
-            
-            if 0 <= na < n and 0<= nb < n and not visited[na][nb]:
-                if l<= abs(graph[a][b] - graph[na][nb]) <= r:
-                    visited[na][nb] = True
-                    q.append((na,nb))
-                    union.append((na,nb))
-    if len(union) <= 1:
-        return 0
-    sm = sum(graph[c][d] for c,d in union) // len(union)
-    for c,d in union:
-        graph[c][d] = sm
-    return 1
-day = 0
-while True:
-    cnt = 0
-    visited = [[False] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
+
+    while queue:
+        x,y = queue.popleft()
+
+        for dx,dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx,ny = x+dx, y+dy
+            if 0 <= nx < N and 0 <= ny < N and ( L <= abs(graph[nx][ny] - graph[x][y]) <= R) and not v[nx][ny]:
+                queue.append((nx,ny))
+                v[nx][ny] = True
+                union.append((nx,ny))
+
+    if len(union) > 1:
+        return union
+    else:
+        return False
+
+
+def check():
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    u = []
+    for i in range(N):
+        for j in range(N):
             if not visited[i][j]:
-                cnt += bfs(i,j,visited)
-    if cnt == 0:
+                result = bfs(i,j,visited)
+                if result:
+                    u.append(result)
+    return u
+
+def share(arr):
+    for tmp in arr:
+        calc = sum([graph[x][y] for x,y in tmp])
+        avg = calc//len(tmp)
+
+
+        for x,y in tmp:
+            graph[x][y] = avg
+
+i = 0
+while True:
+    tmp = check()
+    if len(tmp) == 0:
         break
-    day += 1
-print(day)
+    else:
+        share(tmp)
+        i+=1
+
+print(i)

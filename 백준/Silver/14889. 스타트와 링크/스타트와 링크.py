64929e934ac3300a5206f4d22672a1a3478928d0
Yippi <57821687+YPYP333YPYP@users.noreply.github.com>
2025-06-14 12:29:48 +0900

    [Silver I] Title: 스타트와 링크, Time: 1356 ms, Memory: 113656 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,36 +1,39 @@
-N = int(input()) 
+N = int(input())
 
-status = [list(map(int,input().split())) for _ in range(N)] 
+status = []
+
+for _ in range(N):
+    tmp = list(map(int, input().split()))
+    status.append(tmp)
+
 
 visited = [False] * N
 result = float('inf')
 
-def dfs(a, idx):
+def dfs(n, idx):
     global result
-    
-    if a == N//2:
-    	
-        team_start = 0
-        team_link = 0
-        
-        for i in range (N):
-            for j in range (N):
-            	
-                if visited[i] and visited[j]: 
-                    team_start += status[i][j]
-                    
-                elif not visited[i] and not visited[j]: 
-                    team_link += status[i][j]
-        
-        result = min(result, abs(team_start-team_link))
+
+    if n == N//2:
+
+        team_s = 0
+        team_l = 0
+
+        for i in range(N):
+            for j in range(N):
+                if visited[i] and visited[j]:
+                    team_s += status[i][j]
+                elif not visited[i] and not visited[j]:
+                    team_l += status[i][j]
+
+        result = min(result, abs(team_s - team_l))
         return
-        
-    else: 
-        for i in range(idx, N):
+    else:
+        for i in range(idx,N):
             if not visited[i]:
                 visited[i] = True
-                dfs(a+1, i+1)
+                dfs(n+1, i+1)
                 visited[i] = False
 
-dfs (0,0)
+
+dfs(0,0)
 print(result)
